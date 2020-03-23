644577de11f601f46bee08413046e9bfec256d7c
Sebastian Ortiz <sortizs@eafit.edu.co>
2020-03-23 17:47:08 -0500

    db configuration

--- html/wp-config.php
+++ html/wp-config.php
@@ -23,7 +23,7 @@
 define( 'DB_NAME', 'wordpress' );
 
 /** MySQL database username */
-define( 'DB_USER', 'cms28' );
+define( 'DB_USER', 'root' );
 
 /** MySQL database password */
 define( 'DB_PASSWORD', 'equipo28cms.' );
